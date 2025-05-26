760abf6c36fc4e11ba0e1ffd7637d494e4e45670
deffryap <deffryap@gmail.com>
2025-05-26 20:12:17 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <p className="font-bold bg-red-500">HELLO WORLD</p>
+    </>
+  )
+}
+
+export default App
